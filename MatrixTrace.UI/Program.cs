de0511f66c1ac7bc2a44c8e4affbdfacb5f7711d
Ivan Goncharov <vandrass@gmail.com>
2021-12-07 21:17:05 +0200

    StyleCop and DependencyInjection Added

--- MatrixTrace.UI/Program.cs
+++ MatrixTrace.UI/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace MatrixTrace.UI
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Ivan Goncharov">
+// Copyright (c) Ivan Goncharov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MatrixTrace.UI
 {
     using Microsoft.Extensions.DependencyInjection;
 
     internal class Program
     {
-        static void Main()
+        internal static void Main()
         {
             ServiceCollection serviceCollection;
         }
