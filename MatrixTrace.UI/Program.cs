1d39300a8ae1992f8829b9f0ff5bf588d050fa7f
Ivan Goncharov <vandrass@gmail.com>
2021-12-07 21:04:15 +0200

    Add project files.

---
+++ MatrixTrace.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixTrace.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
