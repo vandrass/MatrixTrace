cd8c8ecf278e1c0c6bfa32a3f1e8ce6ff91dd8c3
Ivan Goncharov <vandrass@gmail.com>
2021-12-12 19:58:23 +0200

    First test is ready

---
+++ MatrixTrace.UnitTests/MatrixActions.Test.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MatrixActionsTests.Test.cs" company="Ivan Goncharov">
+// Copyright (c) Ivan Goncharov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MatrixTrace.UnitTests
+{
+    using MatrixTrace.Application;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MatrixActionsTests
+    {
+        private readonly ServiceCollection _serviceCollection = new ServiceCollection();
+        private ServiceProvider _provider;
+        private IMatrixActions _service;
+
+        /// <summary>
+        /// Method build service by IMatrixActions and MatrixActions, for using by tests.
+        /// </summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _serviceCollection.AddScoped<IMatrixActions, MatrixActions>();
+            _provider = _serviceCollection.BuildServiceProvider();
+            _service = _provider.GetRequiredService<IMatrixActions>();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            // Arrange
+            int[,] matrix = { { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 4, 5, 6, 7 } };
+            Matrix inputMatrix = new Matrix(matrix);
+            int expected = 9;
+
+            // Act
+            var actual = _service.GetMatrixTrace(inputMatrix);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
