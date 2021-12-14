139bed2135469d349f55ccaaee84c5d17bcab1dc
Ivan Goncharov <vandrass@gmail.com>
2021-12-14 19:59:22 +0200

    Matrix clas documented

--- MatrixTrace.Application/Matrix.cs
+++ MatrixTrace.Application/Matrix.cs
@@ -8,22 +8,39 @@ namespace MatrixTrace.Application
 {
     using System;
 
+    /// <summary>
+    /// Matrix object, that build random matrix by input rows and columns number.
+    /// And can to receive a ready matrix (for tests using).
+    /// MinRandValue and MaxRandValue - interval for random numbers.
+    /// </summary>
     public class Matrix
     {
         private const int MinRandValue = 1;
         private const int MaxRandValue = 9;
         private int[,] _matrix;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Matrix"/> class by ready matrix.
+        /// </summary>
+        /// <param name="matrix">multidimensional int array.</param>
         public Matrix(int[,] matrix)
         {
             _matrix = matrix;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Matrix"/> class.
+        /// </summary>
+        /// <param name="rowsNumber">number of matrix rows.</param>
+        /// <param name="columnNumber">number of matrix columns.</param>
         public Matrix(int rowsNumber, int columnNumber)
         {
             FillMatrix(rowsNumber, columnNumber);
         }
 
+        /// <summary>
+        /// Gets matrix.
+        /// </summary>
         public int[,] GetMatrix
         {
             get
