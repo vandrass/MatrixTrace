83e864471289c327df90b1ac87013d64a8928ab8
Ivan Goncharov <vandrass@gmail.com>
2021-12-14 19:41:26 +0200

    IMatrixActions documented

--- MatrixTrace.Application/IMatrixActions.cs
+++ MatrixTrace.Application/IMatrixActions.cs
@@ -6,12 +6,29 @@
 
 namespace MatrixTrace.Application
 {
+    /// <summary>
+    /// interface for implementing actions on a matrix.
+    /// </summary>
     public interface IMatrixActions
     {
+        /// <summary>
+        /// Matrix Output by rows and columns.
+        /// </summary>
+        /// <param name="matrix">Matrix object For output.</param>
         void PrintMatrix(Matrix matrix);
 
+        /// <summary>
+        /// Should Calculate Matrix Trace and will Return an Answer.
+        /// </summary>
+        /// <param name="matrix">Matrix object for calculating.</param>
+        /// <returns>matrix trace.</returns>
         int GetMatrixTrace(Matrix matrix);
 
+        /// <summary>
+        /// Must to build a snake string by input matrix.
+        /// </summary>
+        /// <param name="matrix">Matrix object for snake building. </param>
+        /// <returns>string with finished snake from input matrix.</returns>
         string GetMatrixSnake(Matrix matrix);
     }
 }
